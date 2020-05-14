3c747e2b3f9670e8b6b8a1f133ce77da6d4f0156
Mayank Gupta <mayankgbrc@gmail.com>
2020-05-15 00:37:11 +0530

    Fixed bug of multiple user with same email

--- blog/forms.py
+++ blog/forms.py
@@ -6,15 +6,24 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.forms import UserCreationForm
 import time
+from django.core.exceptions import ValidationError
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True,)
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(u'Email addresses must be unique.')
+        return email
+
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
 
 class PhotoForm(forms.ModelForm):
     x = forms.FloatField(widget=forms.HiddenInput())
